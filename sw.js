98753dfb352dd65645f6e138c73d44e0b7c94c7a
Paulo Gama <DreamerJPMG@gmail.com>
2026-01-27 17:10:13 -0300

    Update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // SuporteApp Web - Service Worker
-// Versão 3.8.4
-const CACHE_NAME = "suporteapp-v3.8.4";
+// Versão 3.8.5
+const CACHE_NAME = "suporteapp-v3.8.5";
 const urlsToCache = [
   "./",
   "./index.html",
